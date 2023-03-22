ea296101c89878b984aa7f965a49efa12ab148ca
Ryan Hood <114943155+ryanhood10@users.noreply.github.com>
2023-03-22 13:04:00 -0500

    added resume

---
+++ shellproject/src/components/pages/Resume.js
@@ -0,0 +1,38 @@
+import React from "react"
+import PDF from "../assets/resume.pdf"
+
+function Resume(){
+   return(
+    <div className="resume">
+          <h2>Resume Skills</h2>
+          <a> My Resume </a>
+        <div className="proficiencies">
+          <div>
+          <h3>Front-End Development Skills</h3>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+             <li>Jquery</li>
+              <li>React</li>
+              
+            </ul>
+          </div>
+          <div>
+            <h3>Back End Development Skills</h3>
+              <ul>
+                
+                <li>Express</li>
+                <li>Node.js</li>
+                <li>REST APIs</li>
+                <li>MongoDB</li>
+               
+              </ul>
+          </div>
+
+        </div>
+      </div>
+   ) 
+}
+
+export default Resume;
