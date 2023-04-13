bff4137b46ef52ee6b9aa93f4a04c634f548f113
Ryan Hood <114943155+ryanhood10@users.noreply.github.com>
2023-04-13 18:01:01 -0500

    changed About me page

--- shellproject/src/components/pages/About.js
+++ shellproject/src/components/pages/About.js
@@ -7,7 +7,15 @@ function About(){
     return(
         <div className="about">
             <h4 className="name-header">Ryan Hood</h4>
-            <p>I am a software developer based out of Nashville Tennesee with a background in full-stack development.</p>
+            <p>Hey, I'm Ryan. I'm a Full-Stack Software Developer from Nashville, Tennessee, with a focus on JavaScript. I graduated from Vanderbilt University's Full-Stack Coding Program, and I have hands-on experience with REST APIs, MongoDB, Express, React, and Node.js. I love building cool and dynamic applications that make a difference.
+<br></br>
+I consider myself a problem solver who's great at time management and communication. When it comes to work, I believe in a strong work ethic, always striving for quality, and never stopping learning. My continuous learning journey includes various platforms like YouTube, Udemy, iTalki, Google Certification and online courses that help me stay up-to-date with the latest technologies.
+<br></br>
+When I'm not coding, you can find me hiking, learning Spanish, trading crypto, or playing basketball with friends. I really enjoy being part of a team and collaborating on projects. Reading "The 5 Dysfunctions of a Team" and drawing from my customer service background have given me a fresh perspective on how to work effectively with others.
+<br></br>
+Feel free to check out my portfolio to see the skills I've honed and the projects I've worked on. I'm always up for a new challenge or an interesting collaboration.
+<br></br>
+Some books that have left an impression on me are "Rich Dad Poor Dad," "The Richest Man in Babylon," "Tuesdays with Morrie," and I'm currently exploring the book "Simplicity" to improve my thinking as a programmer.</p>
         </div>
     )
 }
