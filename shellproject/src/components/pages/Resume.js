9046be03ef545c9673fb389c99525bc7674e018d
Ryan Hood <114943155+ryanhood10@users.noreply.github.com>
2023-05-10 22:10:39 -0500

    added Resume link and styled button for Resume.js

--- shellproject/src/components/pages/Resume.js
+++ shellproject/src/components/pages/Resume.js
@@ -6,7 +6,14 @@ function Resume(){
    return(
     <div className="resume">
           <h2>Resume</h2>
-     
+          
+          <div>
+          <h3>Click here for a Word Document Resume via OneDrive:</h3>
+        <a href="https://1drv.ms/w/s!AoSVg1fJ-_Nwo3e0PoiN-NS-R6qq" download   className="cool-button">
+         View Resume
+        </a>
+        </div>
+
         <div className="proficiencies">
           <div>
           <h3>Front-End Development Skills</h3>
